6a8e72d04ae715e0dc88416c6e783763bddf1d9c
Marcos <terceiro.marcos2005@gmail.com>
2025-04-07 22:32:51 -0300

    removendo arquivo com server pra stripe separado e juntando tudo em um só

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const db = require("./database.js");
 const cors = require("cors");
+const stripe = require("./stripe.js");
 
 const app = express();
 
@@ -139,6 +140,38 @@ app.post("/pagamentos", (req, res) => {
     }
   );
 });
+
+
+// Rota POST - Criar pagamento com Stripe
+app.post('/stripe/criar-pagamento', async (req, res) => {
+    const { valor, nomeInquilino } = req.body;
+  
+    try {
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [
+          {
+            price_data: {
+              currency: 'brl',
+              product_data: {
+                name: `Aluguel - ${nomeInquilino}`,
+              },
+              unit_amount: Math.round(valor * 100),
+            },
+            quantity: 1,
+          },
+        ],
+        mode: 'payment',
+        success_url: 'https://youtube.com',
+        cancel_url: 'https://google.com',
+      });
+  
+      res.json({ url: session.url, sessionId: session.id });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ erro: 'Erro ao criar sessão de pagamento' });
+    }
+  });
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
