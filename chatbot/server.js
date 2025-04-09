87d76820add62fb5dbc28e4de5edd868487d2de5
Marcos <terceiro.marcos2005@gmail.com>
2025-04-09 16:55:18 -0300

    adicionando fetch da docs da z-api

--- chatbot/server.js
+++ chatbot/server.js
@@ -7,7 +7,6 @@ const axios = require("axios");
 const app = express();
 app.use(express.json());
 
-
 // Receber mensagens (POST)
 app.post("/webhook", async (req, res) => {
   const event = req.body.event;
@@ -28,10 +27,23 @@ app.post("/webhook", async (req, res) => {
       resposta = "Olá! Como posso te ajudar?";
 
     // Enviar resposta via Z-API
-    await axios.post(`https://api.z-api.io/instances/3DF7A08EBAE0F00686418E66062CE0C1/token/7C15DC72E37255AD095DB505/send-text`, {
-      phone: from,
-      message: resposta,
-    });
+    fetch(
+      "https://api.z-api.io/instances/3DF7A08EBAE0F00686418E66062CE0C1/token/7C15DC72E37255AD095DB505/send-text",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "client-token": "F775e0c83a317429f93eefaa27ba21c03S",
+        },
+        body: JSON.stringify({
+          phone: from, 
+          message: resposta,
+        }),
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => console.log("Resposta da API:", data))
+      .catch((err) => console.error("Erro ao enviar mensagem:", err));
 
     console.log("Resposta enviada:", resposta);
   }
@@ -40,6 +52,6 @@ app.post("/webhook", async (req, res) => {
 });
 
 // Iniciar servidor
-app.listen(process.env.PORT||3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Servidor rodando na porta 3000");
 });
