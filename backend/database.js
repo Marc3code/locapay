6a4a4891ad13727cfe968a2a477ea8c08adfdfbd
Marcos <terceiro.marcos2005@gmail.com>
2025-04-11 12:30:52 -0300

    abrindo pool de conexões e removendo .conect

--- backend/database.js
+++ backend/database.js
@@ -15,12 +15,5 @@ const connection = db.createPool({
 
   });
 
-connection.connect((err) => {
-    if (err) {
-        console.error('Error connecting to the database:', err);
-        return;
-    }
-    console.log('Connected to the database');
-});
 
 module.exports = connection;
